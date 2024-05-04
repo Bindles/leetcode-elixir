a7cb931b4d40971f62eee5e3833c552fcc227f6b
Bindles <bindlez@gmail.com>
2024-05-03 23:46:44 -0400

    3019. Number of Changing Keys-ELIXIR

---
+++ 3019. Number of Changing Keys.exs
@@ -0,0 +1,17 @@
+#* 3019. Number of Changing Keys
+s = "aAbBcC"
+defmodule Solution do
+  @spec count_key_changes(s :: String.t) :: integer
+  def count_key_changes(s) do
+    [x| xs] = s
+      |> String.downcase()
+      |> String.graphemes()
+
+    Enum.reduce(xs, {0, x}, fn x, {acc, prev} ->
+        acc = acc + if x == prev, do: 0, else: 1
+        {acc, x}
+    end)
+    |> elem(0)
+  end
+end
+IO.puts(Solution.count_key_changes(s))
